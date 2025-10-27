746e5c35b64c5bb91652d77f91d161b3c198ef78
Rohan Sheth <rohanjaiminsheth@gmail.com>
2025-10-26 23:20:27 -0700

    made mobile changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="mt-auto py-12">
       <div className="text-center">
-        <div className="flex items-center justify-center gap-4 mb-4 text-[#3d2b22] opacity-50 ">
+        <div className="flex items-center justify-center gap-4 mb-4 opacity-50 ">
         </div>
         
         <p className="text-sm">© 2025 Rohan Sheth</p>
