fdb6a462c1714338ac0a9c4538b3aff61941d285
Rohan Sheth <rohanjaiminsheth@gmail.com>
2025-10-26 21:42:41 -0700

    add disclaimer

--- src/components/homebody.tsx
+++ src/components/homebody.tsx
@@ -5,7 +5,7 @@ function Body() {
             <p>I'm Rohan Sheth, a Computer Science and Engineering student at the University of California Davis 
                 interested in AI, policy, and scalable development. Here you can find my 
                 <a href="/rohan-website/Rohan_s_Resume_Main.pdf" target="_blank" rel="noopener noreferrer"> resume </a>  
-                and some blogposts.</p>
+                and (soon) some blogposts.</p>
         </main>
     )
 }
