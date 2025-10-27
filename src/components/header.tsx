9bef7115c1bec84976aa53a6d7652cd2ba17541a
Rohan Sheth <rohanjaiminsheth@gmail.com>
2025-10-26 23:42:52 -0700

    header link changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ return (
     <div className="absolute top-0 left-0 w-full p-4">
         <h1>Rohan Sheth</h1>
         <div className="flex items-center gap-7" id="links">
-            <a href="/blog">Blog</a>
-            <a href="/about">About</a>
+            <a href="/rohan-website/">Blog</a>
+            <a href="/rohan-website/">About</a>
             <a href="mailto:rohanjaiminsheth@gmail.com">Contact</a>
       </div>
     </div>
